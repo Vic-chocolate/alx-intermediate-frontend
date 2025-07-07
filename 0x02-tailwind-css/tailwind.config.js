1d31331b6afe5afe2a7301af21cf7565288c7e64
cunnan <vs807058@gmail.com>
2025-07-07 20:06:51 +0200

    moved 1-index.html back to 0x00-tailwind-css

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust path to match your project structure
+    "./**/*.{html,js,jsx,ts,tsx}", // Adjust path to match your project structure
     "./public/index.html",
     "../0x02-tailwind-css/**/*/.{html, js, jsx, ts,tsx}",
   ],
