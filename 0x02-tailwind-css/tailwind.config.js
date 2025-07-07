79a7aa5b0923f18b5189c51f1adf50a72504f044
cunnan <vs807058@gmail.com>
2025-07-07 20:03:03 +0200

    added 1-index.html inside src

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust path to match your project structure
+    "./public/index.html",
+    "../0x02-tailwind-css/**/*/.{html, js, jsx, ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
